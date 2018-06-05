08b83c307fa65b2ee933a934f2c5a9e36d815fa7
cocowaszak <colettewaszak@gmail.com>
2018-06-05 16:49:50 +0000

    automatically backed up by learn

--- methods_arguments.rb
+++ methods_arguments.rb
@@ -1,5 +1,17 @@
 def greeting
   name = "Coco"
 puts "My name is #{name}"
-end
+#end
+#greeting
+#method called greeting. spits out whatever name I assign. method has to be called at the end in order to return
+
+greeting ("Hey", "Michelle", "Dayi", "morning")
 greeting
+#Hey, Dayi, I'm Michelle, hows you're morning?
+
+
+def greeting (local_greeting, your_name, my_name, local_time)
+ local_greeting = "Hi" your_name = "Dayi" my_name = "Coco" local_time = "morning"
+  puts "#{local_greeting} #{your_name}, I'm #{my_name}, how's your #{local_time}?"
+end
+greeting ("Hey", "Dayi", "Coco", "morning")
