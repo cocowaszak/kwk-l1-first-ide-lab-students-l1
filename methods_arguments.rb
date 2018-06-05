0a748c56be4f3b38143b86962d1d8631664b6586
cocowaszak <colettewaszak@gmail.com>
2018-06-05 17:00:27 +0000

    automatically backed up by learn

--- methods_arguments.rb
+++ methods_arguments.rb
@@ -1,23 +1,28 @@
-def greeting
-  name = "Coco"
-puts "My name is #{name}"
+#def greeting
+#name = "Coco"
+#puts "My name is #{name}"
 #end
 #greeting
 #method called greeting. spits out whatever name I assign. method has to be called at the end in order to return
 
-greeting ("Hey", "Michelle", "Dayi", "morning")
-greeting
+#greeting ("Hey", "Michelle", "Dayi", "morning")
+#greeting
 #Hey, Dayi, I'm Michelle, hows you're morning?
 
 
-def greeting (local_greeting, your_name, my_name, local_time)
- local_greeting = "Hi" your_name = "Dayi" my_name = "Coco" local_time = "morning"
-  puts "#{local_greeting} #{your_name}, I'm #{my_name}, how's your #{local_time}?"
+#def greeting (local_greeting, your_name, my_name, local_time)
+ #local_greeting = "Hi" your_name = "Dayi" my_name = "Coco" #local_time = "morning"
+  #puts "#{local_greeting} #{your_name}, I'm #{my_name}, how's your #{local_time}?"
+#end
+#greeting("Hey", "Dayi", "Coco", "morning")
+
+
+#def greeting (my_name, my_greeting = "Hi")
+#puts "#{my_greeting}! I'm #{my_name}"
+#end 
+#greeting("Karlie")
+
+def gradebook (student_name, grade)
+  puts "Hello #{student_name}, your grade in this class is a #{grade}"
 end
-greeting("Hey", "Dayi", "Coco", "morning")
-
-
-def greeting (my_name, my_greeting = "Hi")
-  puts "#{my_greeting}! I'm #{my_name}"
-end 
-greeting("Karlie")
+gradebook("Ferris", "F")
